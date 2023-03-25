f471d35294ccdff2a2e21f253a49f387d4993cbb
Juan Pablo BAutista <jbautistaclavijo@gmail.com>
2023-03-25 16:52:39 -0500

    Inicializando el proyecto

---
+++ src/main/java/veterinaria/veterinaria/Veterinaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package veterinaria.veterinaria;
+
+/**
+ *
+ * @author juanpbaucl514
+ */
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
